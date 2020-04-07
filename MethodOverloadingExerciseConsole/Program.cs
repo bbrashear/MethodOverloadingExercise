8ba393d321770446e7730e9034418c3cb636760a
Blake Brashear <bbrashear94@gmail.com>
2020-04-07 10:58:53 -0400

    Initial commit - completed exercises

--- MethodOverloadingExerciseConsole/Program.cs
+++ MethodOverloadingExerciseConsole/Program.cs
@@ -6,7 +6,51 @@ namespace MethodOverloadingExerciseConsole
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("How many dollars are in your pocket?");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("How many dollars are in your friend's pocket?");
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine("True or False: Do you like money?");
+            bool isTrue = bool.Parse(Console.ReadLine());
+            Console.WriteLine(Add(a, b, isTrue));
+
+
+
+        }
+
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static double Add(double a, double b)
+        {
+            return a + b;
+        }
+
+        public static string Add(int a, int b, bool isTrue)
+        {
+
+            if (isTrue)
+            {
+                int c = a + b;
+
+                if (c == 1)
+                {
+                    return $"{c} dollar";
+                }
+                else
+                {
+                    return $"{c} dollars";
+                }
+            }
+            else
+            {
+                return "I guess you don't like money";
+            }
+
+
         }
     }
 }
